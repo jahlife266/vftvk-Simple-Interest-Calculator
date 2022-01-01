94322d426fb4f0e4840e3fcf0781681c7d55e891
jahlife266 <88982022+jahlife266@users.noreply.github.com>
2021-12-31 21:02:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * 20 * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-  
-function updateRate() 
-{
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    if(principal<=0)
+        alert('Enter a positive number please!');
+    else
+    {
+        var output = "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>";
+        document.getElementById("result").innerHTML=output;
+    }
+    
     
 }
+
+function updateRate()
+    {
+        var rateVal = document.getElementById("rate").value;
+        console.log(rateVal);
+        document.getElementById("rate_val").innerText=rateVal;
+    }
         
